7b15844d70acc31ba75cd820a9a9af1935d242f4
TeeZeeeh <63507505+TeeZeeeh@users.noreply.github.com>
2022-11-13 15:37:21 +0600

    5th kyu kata

---
+++ move_zeroes_to_end.py
@@ -0,0 +1,16 @@
+# Code by Tajrina Promela
+# Date 13th November 2022, 2:30 PM
+# Program that recursively returns the sum of digits until it's a single digit
+
+def move_zeroes(lst):
+    res = []
+    zeroes = []
+    for item in lst:
+        if item == 0:
+            zeroes.append(item)
+        else:
+            res.append(item)
+    res.extend(zeroes)
+    return res      
+
+print(move_zeroes([1, 0, 1, 2, 0, 1, 3]))
